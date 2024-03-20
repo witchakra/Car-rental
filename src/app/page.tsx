7fd4091a7c839b7ce5d510447e333e91d868af60
witchakra <131113499+witchakra@users.noreply.github.com>
2024-03-20 15:31:09 +0700

    Ass8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Banner from '@/components/Banner'
 import ProductCard from '@/components/ProductCard'
 import styles from './page.module.css'
 import CarPanel from '@/components/CarPanel'
+import { TravelCard } from '@/components/TravelCard'
 
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <TravelCard></TravelCard>
     </main>
   )
 }
--- src/components/CarPanel.tsx
+++ src/components/CarPanel.tsx
@@ -4,8 +4,12 @@ import { useReducer } from "react"
 import ProductCard from "./ProductCard"
 import Link from "next/link"
 import { link } from "fs"
+import { useRef } from "react"
 
 export default function CarPanel (){
+
+    const countRef = useRef(0)
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const compareReducer = (compareList:Set<string>,action:{type:string,carName:string}) =>{
         switch(action.type){
@@ -44,6 +48,20 @@ export default function CarPanel (){
             </div>
             <div className="w-full text-xl font-medium">Compare List :{compareList.size}</div>
             {Array.from(compareList).map((car)=> <div key={car} onClick={() =>dispatchCompare({type:'remove',carName:car})}>{car}</div> )}
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
+            text-white shadow-sm" onClick={() => {countRef.current = countRef.current+1; alert(countRef.current)}}>
+                Count with Ref Object
+            </button>
+
+            <input type="text" placeholder="Please fill" className="block text-gray-900 text-sm rounded-lg p-2 m-2 bg-purple-50
+            ring-1 ring-inset ring-purple-400 focus:outline-none focus:bg-purple-200 focus:ring-2"
+            ref = {inputRef}/>
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
+            text-white shadow-sm" onClick={() => {if(inputRef.current != null) inputRef.current.focus()}}>
+                Focus Input
+            </button>
         </div>
     )
 }
