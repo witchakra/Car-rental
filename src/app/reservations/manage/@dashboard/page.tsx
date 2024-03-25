bbf6f6de64fafd0b87fb828f04318131217b5bc7
witchakra <131113499+witchakra@users.noreply.github.com>
2024-03-25 21:36:59 +0700

    Ass10

--- src/app/reservations/manage/@dashboard/page.tsx
+++ src/app/reservations/manage/@dashboard/page.tsx
@@ -1,7 +1,23 @@
-export default function DashboardPage(){
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import getUserProfile from "@/libs/getUserProfile"
+
+export default async function DashboardPage(){
+
+    const session = await getServerSession(authOptions)
+    if(!session || !session.user.token) return null
+
+    const proflie = await getUserProfile(session.user.token)
+    var createdAt =new Date(proflie.data.createdAt)
+
     return (
-        <main className="bg-slate-100">
-            <div>Your Dashboard</div>
+        <main className="bg-slate-100 m-5 p-5">
+            <div className="text-2xl">{proflie.data.name}</div>
+            <table className="table-auto border-separate border-spacing-2 "><tbody>
+                <tr><td>Email</td><td>{proflie.data.email}</td></tr>
+                <tr><td>Tel.</td><td>{proflie.data.tel}</td></tr>
+                <tr><td>Member Since</td><td>{createdAt.toString()}</td></tr>
+            </tbody></table>
         </main>
     )
 }
